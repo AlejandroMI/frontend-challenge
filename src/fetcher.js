1e618c7a5562f8665e9de9ab4ee83e20cf0e583f
AM <alexmarco@gmx.co.uk>
2019-06-27 14:25:53 +0100

    Write fn in fetch to get movie results by keyword

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,20 +1,35 @@
 import axios from "axios";
 
+const keyAPI = "eae120b2b421dd7480174a3dbda3bb89";
+
 // All of your API requests should be in this file
 // i.e.
 // export const getMovieGenres = async () => {
 
 // };
 
-//Getting popular movies
+//Get popular movies
 export const getPopularMovies = async () => {
   try {
     const response = await axios.get(
-      "https://api.themoviedb.org/3/trending/movie/week?api_key=eae120b2b421dd7480174a3dbda3bb89"
+      `https://api.themoviedb.org/3/trending/movie/week?api_key=${keyAPI}`
     );
     console.log("Success in: " + getPopularMovies.name);
     return response.data.results;
   } catch (error) {
     console.log("Error in:" + getPopularMovies.name + "\n" + error);
+  }
+};
+
+//Get movies by keyword
+export const getMoviesByKeyword = async (keyword, year) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?query=${keyword}&api_key=${keyAPI}`
+    );
+    console.log("Success in: " + getMoviesByKeyword.name);
+    return response.data.results;
+  } catch (error) {
+    console.log("Error in:" + getMoviesByKeyword.name + "\n" + error);
   }
 };
