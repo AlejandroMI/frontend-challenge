6e60009caaa027d397bc3f32a7f0070cf4a6bc9d
Alejandro Marco <alexmarco@gmx.co.uk>
2020-06-22 09:09:33 +0100

    Updates

---
+++ precache-manifest.4620a0203dbdb76bfaf410218d8bcb2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7059e351f2fdfac3152393a9949b1f",
+    "url": "/movie-finder/index.html"
+  },
+  {
+    "revision": "de44c60933f330ffcdaa",
+    "url": "/movie-finder/static/css/main.df26aff6.chunk.css"
+  },
+  {
+    "revision": "8038b1cc1f2bbf1f45f3",
+    "url": "/movie-finder/static/js/2.7abfc7f1.chunk.js"
+  },
+  {
+    "revision": "de44c60933f330ffcdaa",
+    "url": "/movie-finder/static/js/main.243f522d.chunk.js"
+  },
+  {
+    "revision": "a667e2b1cde792e0d9a3",
+    "url": "/movie-finder/static/js/runtime~main.e473438c.js"
+  },
+  {
+    "revision": "69da28d9340f2111ea91a1c6fbba4431",
+    "url": "/movie-finder/static/media/minus-symbol.69da28d9.svg"
+  },
+  {
+    "revision": "be7401cac1c44f032ddcc93a1cc21c70",
+    "url": "/movie-finder/static/media/plus-symbol.be7401ca.svg"
+  }
+]);
--- src/fetcher.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-// All of your API requests should be in this file
-// i.e.
-// export const getMovieGenres = async () => {
-
-// };
